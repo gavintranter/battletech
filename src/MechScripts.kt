2418b465893e70ee49ccf62f815c80998eef9268
Gavin Tranter <gavintranter@users.noreply.github.com>
2019-01-08 14:03:38 +0000

    Extract basic mech data
    Some duplication with the SystemsScripts to be taken care of.

---
+++ src/MechScripts.kt
@@ -0,0 +1,57 @@
+package uk.trantr.battletech
+
+import java.io.File
+import java.io.StringReader
+import java.util.*
+
+private enum class Class {
+    Light,
+    Medium,
+    Heavy,
+    Assault;
+
+    companion object Factory {
+        fun from(value: String): Class {
+            return Class.valueOf(value.toLowerCase().capitalize())
+        }
+    }
+}
+
+private data class Mech(val model: String, val name: String, val weightClass: Class, val weight: Int) {
+    override fun toString(): String {
+        return "$model, $name, $weightClass, $weight"
+    }
+
+    companion object {
+        private const val key = "key"
+        private val regex = ".*(\"Name\"|\"Tonnage\"|\"weightClass\"|\"VariantName\"): \"?([[\\\\]-.\\w ']+)\"?,?".toRegex()
+        private val p = Properties()
+
+        private fun extractValue(value: String): String {
+            val escaped = value.replace(regex, "$2")
+            p.load(StringReader("$key=$escaped"))
+
+            return p.getProperty(key)
+        }
+
+        operator fun invoke(lines: List<String>): Mech {
+            val fields = lines.filter { it.matches(regex) }
+                .map { extractValue(it) }
+
+            val name = fields[0]
+            val tonnage = fields[1].toInt()
+            val weightClass = Class.from(fields[2])
+            val variantName = fields[11]
+
+            return Mech(variantName, name, weightClass, tonnage)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val mechs = File("/users/Gavin/Documents/battleTech/Mechs").listFiles().filter { it.extension.equals("json", true) }
+    mechs.map { Mech(it.readLines()) }
+        .sortedBy { it.weight }
+        .forEach{ println(it) }
+
+}
