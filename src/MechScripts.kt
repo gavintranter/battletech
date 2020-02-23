b8c7ebd341d9222c4e8ec8f9e6fbb24338a970a1
Gavin Tranter <gavintranter@users.noreply.github.com>
2020-02-23 12:07:02 +0000

    Add resale and cost to Mech class

--- src/MechScripts.kt
+++ src/MechScripts.kt
@@ -1,6 +1,7 @@
 package uk.trantr.battletech
 
 import java.io.File
+import kotlin.math.roundToInt
 
 fun getAssets(assetName: String): Array<File> = File("/users/Gavin/Documents/battleTech/$assetName")
     .listFiles { name -> name.extension == "json"  } ?: emptyArray()
@@ -27,18 +28,20 @@ private enum class MechClass {
     }
 }
 
-private data class Mech(val model: String, val name: String, val mechClass: MechClass, val tonnage: Int) {
+private data class Mech(val model: String, val name: String, val mechClass: MechClass, val tonnage: Int, val cost: Int) {
+    val resale: Int = ((10.24 / 100) * cost).roundToInt()
+
     override fun toString(): String {
-        return "$model, $name, $mechClass, $tonnage"
+        return "$mechClass, $model, $name, $tonnage, $resale"
     }
 
     companion object {
-        private val regex = "\\s\"[NameTongVrit]{4,11}\"\\s?:\\s?\"?([^0][- \\w]{2,})".toRegex()
+        private val regex = "\\s\"[CostNameTngVri]{4,11}\"\\s?:\\s?\"?([^0][- \\w]{2,})".toRegex()
 
         operator fun invoke(file: File): Mech {
-            val (name, tonnage, variantName) = extractDetails(regex, file)
+            val (cost, name, tonnage, variantName) = extractDetails(regex, file)
 
-            return Mech(variantName, name, MechClass.from(tonnage.toInt()), tonnage.toInt())
+            return Mech(variantName, name, MechClass.from(tonnage.toInt()), tonnage.toInt(), cost.toInt())
         }
     }
 }
