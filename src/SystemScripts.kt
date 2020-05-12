40d76fb83a8b9e203c93b7c743019c18e6531fe4
Gavin Tranter <gavintranter@users.noreply.github.com>
2020-05-12 14:46:24 +0100

    Override equals and hashcode

--- src/SystemScripts.kt
+++ src/SystemScripts.kt
@@ -48,6 +48,18 @@ private data class PlanetarySystem(val name: String, val allegiance: Faction, va
         return if (result == 0) name.compareTo(other.name) else result
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PlanetarySystem
+        return name == other.name
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
     override fun toString(): String {
         return "$name $allegiance $skulls $starLeague"
     }
